d980de748b14deffe6fea65b1d4575e9461d9ec7
Rod5919 <sergiorodrigofernandeztesta@gmail.com>
2022-06-29 19:41:23 -0400

    show annotations with image size

--- utils/show_annotations/main.py
+++ utils/show_annotations/main.py
@@ -5,7 +5,6 @@ import os
 import matplotlib.pyplot as plt
 import xml.etree.ElementTree as ET
 
-
 def return_bndboxes(filename):
     """ Returns a list of BoundingBoxesOnImage objects from an xml file. 
 
@@ -30,6 +29,17 @@ def return_bndboxes(filename):
     return bndboxes
 
 def draw_bndboxes(image, bndboxes, color=(0, 255, 0), thickness=2):
+    """ Returns a list of BoundingBoxesOnImage objects from an xml file.
+
+    Args:
+        image (np.ndarray): An image.
+        bndboxes (list): A list of BoundingBoxes
+        color (tuple, optional): Bounding boxes color. Defaults to (0, 255, 0).
+        thickness (int, optional): Bounding boxes line thickness. Defaults to 2.
+
+    Returns:
+        np.ndarray: An image with bounding boxes drawn.
+    """    
     for bndbox in bndboxes:
         xmin, ymin, xmax, ymax = bndbox
         image = cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, thickness)
@@ -39,12 +49,16 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-I","--input_path", type=str, help="path to input images", default="data")
     parser.add_argument("--image-extensions", type=str, help="image extensions to process", default="jpg")
+    parser.add_argument("--window-size", nargs=2, type=int, help="window size", default=(416, 416))
     
     image_files = glob.glob(parser.parse_known_args()[0].input_path + f"/*.{parser.parse_known_args()[0].image_extensions}")
     annotations = glob.glob(parser.parse_known_args()[0].input_path + f"/*.xml")
     
     image_files.sort()
     annotations.sort()
+    
+    print(parser.parse_known_args()[0].window_size)
+    plt.rcParams["figure.figsize"] = parser.parse_known_args()[0].window_size
     
     for image_file, annotation, plt_index in zip(image_files, annotations, (331, 332, 333, 334, 335, 336, 337, 338, 339)):
         bboxes = return_bndboxes(annotation)
